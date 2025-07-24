da9a15cd981c4528150029b5b44377e9126bf39d
dat0312 <datvipro04@gmail.com>
2025-07-24 22:37:40 +0700

    fix sidebar homepage

--- src/java/elearning/constant/ServerConnectionInfo.java
+++ src/java/elearning/constant/ServerConnectionInfo.java
@@ -15,7 +15,7 @@ public class ServerConnectionInfo {
     public static final String PORT = "3306";
     public static final String DBNAME = "EducationPlatform";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "123456789";
+    public static final String PASSWORD = "123456";
     public static final String CLASS_DRIVER = "com.mysql.cj.jdbc.Driver";
     public static final String CONNECTION_URL = "jdbc:mysql://" + HOSTNAME + ":" + PORT + "/" + DBNAME 
             + "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
