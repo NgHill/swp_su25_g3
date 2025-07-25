38acb42128a8d73886f55aa6086c101e7494d64f
MnhMG <manhmg24112001@gmail.com>
2025-07-25 19:29:42 +0700

    feat: iter 3

--- src/java/elearning/controller/Login.java
+++ src/java/elearning/controller/Login.java
@@ -54,14 +54,15 @@ public class Login extends HttpServlet {
         // Nếu đã đăng nhập rồi thì chuyển hướng sang trang home
         User userAuth = (User) request.getSession().getAttribute("userAuth");
         if (userAuth != null) {
-            if (userAuth.getRole().equals("mtk")) {
-                response.sendRedirect("mtk-dashboard");
-            }
-            else if (userAuth.getRole().equals("admin")) {
-                response.sendRedirect("userlist");
-            }
-            else {
-                response.sendRedirect("home");
+            switch (userAuth.getRole()) {
+                case "mtk" ->
+                    response.sendRedirect("mtk-dashboard");
+                case "admin" ->
+                    response.sendRedirect("userlist");
+                case "courseContent" ->
+                    response.sendRedirect("subject-list2");
+                default ->
+                    response.sendRedirect("home");
             }
             return;
         }
@@ -89,13 +90,15 @@ public class Login extends HttpServlet {
                     // Lưu user vào session
                     request.getSession().setAttribute("userAuth", userAuth);
 
-                    // Kiểm tra role để chuyển hướng
-                    if ("admin".equals(userAuth.getRole())) {
-                        // Nếu là admin, chuyển đến user list
-                        response.sendRedirect("userlist");
-                    } else {
-                        // Nếu không phải admin, chuyển về trang home
-                        response.sendRedirect("home");
+                    switch (userAuth.getRole()) {
+                        case "mtk" ->
+                            response.sendRedirect("mtk-dashboard");
+                        case "admin" ->
+                            response.sendRedirect("userlist");
+                        case "courseContent" ->
+                            response.sendRedirect("subject-list2");
+                        default ->
+                            response.sendRedirect("home");
                     }
                     return;
                 }
